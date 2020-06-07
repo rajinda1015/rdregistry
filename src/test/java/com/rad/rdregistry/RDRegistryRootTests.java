15ea5806c5ac9ef25d03b9a300245c6fea4458e9
rajinda1050 <rajinda.dissanayake@gmail.com>
2020-06-07 20:21:49 +0200

    Initial commit

---
+++ src/test/java/com/rad/rdregistry/RDRegistryRootTests.java
@@ -0,0 +1,13 @@
+package com.rad.rdregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RDRegistryRootTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
